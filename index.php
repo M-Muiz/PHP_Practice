5648c541b3a72ecbc9ed05072fff39f93a48e866
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-11 23:18:29 +0500

    learn basic concepts

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h1>Print Hello world </h1>";
+echo "It's really beautiful day"
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>echo "Hy"</h1>
+</body>
+</html>
