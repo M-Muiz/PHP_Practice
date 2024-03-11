51b197b18c7df6103b3e9d7fda6a070776d2677a
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-11 23:43:13 +0500

    learn php variables

--- index.php
+++ index.php
@@ -1,17 +1,60 @@
 <?php
-echo "<h1>Print Hello world </h1>";
-echo "It's really beautiful day"
+// echo "<h1>Print Hello world </h1>";
+// echo "It's really beautiful day <br>";
+
+// Variables
+
+//String Type Variabels
+
+// $name = "Moiz";
+// $hobby = "Gaming";
+// echo "Hello {$name}";
+// echo "<h2> I like {$hobby} <h2/>"
+
+
+//Number Type Variables
+
+// $age = 10;
+
+// echo "I am {$age} years old<br>";
+
+// $users = 4;
+
+// echo "There are only {$users} online."
+
+// $price = 4.5;
+
+// echo "The price is \${$price}"; impotrant 
+
+
+//Boolean Type Variables
+
+// $quantity = 4;
+// $food = "Burgers";
+// $price = 12;
+// $total = null;
+
+
+// echo "You have orderd {$quantity}x {$food}s <br>";
+// $total = $quantity * $price;
+// echo "The total price is \${$total}";
+
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>echo "Hy"</h1>
+
 </body>
+
 </html>
